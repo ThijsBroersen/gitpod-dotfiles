c99a8df286cb76bbb8d6b8e68c3183f6ab0e10e6
Thijs Broersen <4889512+ThijsBroersen@users.noreply.github.com>
2022-02-16 15:14:27 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 zsh -c "git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
 zsh -c "git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions"
-sudo apt install fonts-powerline -y
+sudo apt install fonts-powerline cloc -y
     
 rm ~/.zshrc || true
 rm ~/.p10k.zsh || true
