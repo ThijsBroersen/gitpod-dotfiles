edab9240e50c061b3d7a4d18e2edc4937c6099cd
Thijs Broersen <4889512+ThijsBroersen@users.noreply.github.com>
2022-03-25 17:49:45 +0100

    Update .gitpodenv.sh

--- .gitpodenv.sh
+++ .gitpodenv.sh
@@ -2,6 +2,8 @@ export PATH="$PATH:/workspace/coursier-apps"
 
 export SBT_OPTS="-Dsbt.global.base=/workspace/cache/sbt/.sbtboot -Dsbt.boot.directory=/workspace/cache/sbt/boot -Dsbt.ivy.home=/workspace/cache/sbt/.ivy -Dsbt.coursier.home=/workspace/cache/sbt/.coursier"
 
+export COURSIER_INSTALL_DIR="/workspace/coursier-apps"
+
 export COURSIER_CACHE="/workspace/cache/coursier"
 
 export COURSIER_JVM_CACHE="/workspace/cache/coursier/jvm"
