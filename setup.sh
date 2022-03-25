bddab16269b0c31a3af20337ccc1ab1915e59726
Thijs Broersen <4889512+ThijsBroersen@users.noreply.github.com>
2022-03-25 16:53:35 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/zsh
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 zsh -c "git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
