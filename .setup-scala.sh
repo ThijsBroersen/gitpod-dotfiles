8e931aaf8ae0304bb7017a70fea135bb3487ec3b
Thijs Broersen <4889512+ThijsBroersen@users.noreply.github.com>
2022-07-11 22:35:18 +0200

    fix cs install

--- .setup-scala.sh
+++ .setup-scala.sh
@@ -2,3 +2,10 @@
 
 mkdir -p ~/.bloop
 echo '{"javaOptions": ["-Xms1G","-Xmx8G","-Xss64m","-XX:ReservedCodeCacheSize=256M","-XX:MaxInlineLevel=20","-XX:+UseParallelGC","-Dfile.encoding=UTF-8"]}' > ~/.bloop/bloop.json
+
+curl -fL https://github.com/coursier/launchers/raw/master/cs-x86_64-pc-linux.gz | gzip -d > cs
+chmod +x cs
+./cs setup --yes
+rm cs
+
+cs install bloop --only-prebuilt=true
